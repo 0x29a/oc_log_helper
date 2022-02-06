04fcebd9c4977edd2af21c4c3772e1d95b4da131
0x29a <grrrr@protonmail.com>
2022-02-06 11:37:13 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.2 (2022-02-06)
+
+Remove redundant dependencies.
+
 # 0.0.1 (2022-02-06)
 
 Initial version.
--- oc_log_helper/__init__.py
+++ oc_log_helper/__init__.py
@@ -1,5 +1,5 @@
 """Let's build a new CLI application."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from oc_log_helper.main import cli  # noqa
